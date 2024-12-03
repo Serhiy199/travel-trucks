26a8ab41cda2456830c2ad00b6724621b29578cf
Sergiy Horodetskiy <bryklin.best.1994@gmail.com>
2024-12-04 00:27:27 +0200

    axios default

--- src/article-api.js
+++ src/article-api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// axios.default.baseURL = "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io";
+axios.defaults.baseURL = 'https://66b1f8e71ca8ad33d4f5f63e.mockapi.io';
 
 export const fetchArticles = async () => {
-    const response = await axios.get('https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers');
+    const response = await axios.get('/campers');
     return response.data.items;
 };
